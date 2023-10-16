e7df46bab19e00dc3bf9c2d07d7ace53ed02c4bb
Sebastian <seb154@live.com>
2023-10-16 09:36:57 -0400

    Adds .results.json and modifies index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return "This one is on me!";
+  } else if (someValue > 2500) {
+    return "No can do.";
+  } else if (someValue > 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else if (someValue > 400) {
+    return "That will be twenty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someValue) {
+  if (someValue === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
